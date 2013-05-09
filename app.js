f3a7724d74cb0abdb137105e0e546459e65442f6
YoungSoul <youngsoul@inbox.com>
2013-05-08 21:39:48 -0500

    initial

---
+++ app.js
@@ -0,0 +1,110 @@
+
+/**
+ * Module dependencies.
+ */
+var flash = require('connect-flash')
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+var passport = require('passport');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+var LocalStrategy = require('passport-local').Strategy;
+var login = require('./routes/login');
+var photos = require('./routes/photos');
+var os = require('os');
+
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser('secret'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({secret: 'secret secret'})); // must be before passport.session
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//--------------  setup passport  ----------------
+passport.use(new LocalStrategy( function(username, password, done) {
+  console.log("username: " + username + " password: " + password);
+  if( username == "foo" && password == "bar") {
+    console.log("authenticated");
+    return done(null, {username: username, firstname: 'Bob', lastname: 'Builder'});
+
+  } else {
+    console.log("failed auth");
+    return done(null, false, {message: 'Incorrect login'});
+  }
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+
+});
+
+
+//-------------------------------  Setup Routes ------------
+//app.get('/', routes.index);
+//app.get('/', passport.authenticate('local', {successRedirect: '/photos',
+//    failureRedirect: '/login',
+//    failureFlash: true}));
+
+app.get('/', ensureLoggedIn('/login', function(req,res) {
+  res.send('Hello ' + req.user.username);
+}));
+
+app.get('/users', user.list);
+app.get('/login', login.login);
+app.get('/logout', function(req,res) {
+  req.logout();
+  res.redirect('/');
+});
+app.get('/photos', ensureLoggedIn('/login'), function(req,res) {
+  photos.list(req,res);
+});
+
+//app.get('/photos', ensureLoggedIn('/login', photos.list));
+//app.get('/photos', photos.list);
+
+
+app.post('/login', passport.authenticate('local', {successRedirect: '/photos',
+                                                   failureRedirect: '/login',
+                                                   failureFlash: true}));
+
+
+//--- find host ip address
+var hostIpAddress = "";
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family=='IPv4') {
+      hostIpAddress = details.address
+      console.log(dev+(alias?':'+alias:''),details.address);
+      ++alias;
+    }
+  });
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/photos.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+var photoProperties = {}
+photoProperties['cam1'] = "/Users/youngsoul/DevData/dir1";
+
+var fileName = "foo.txt";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          console.log(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+
+
+exports.list = function(req, res){
+  getDailyPhotoSummary(req,res,'cam1');
+  res.render('./photos/index', { title: 'Photos here' });
+};
+
+var getDailyPhotoSummary = function(req,res,cameraName) {
+
+  fs.readdir(photoProperties[cameraName], function(err, files) {
+    for( var i = 0; i < files.length; i++) {
+      console.log("file: " + files[i]);
+    }
+  });
+}
